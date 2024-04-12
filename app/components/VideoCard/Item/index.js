0107d8555cfa84da1b2836055835c28b64902c01
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-04-12 16:17:24 +0700

    show Like button and convert component to hook

--- app/components/VideoCard/Item/index.js
+++ app/components/VideoCard/Item/index.js
@@ -1,89 +1,82 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { Card, CardBody, CardTitle } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import MovieAPI from '../../../api/backend/movies';
 import UserUtils from '../../../utils/user/UserUtils';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import { Button } from '@material-ui/core';
 import { faListAlt, faHeading, faMoneyBillAlt, faBuilding } from '@fortawesome/free-solid-svg-icons';
+
 library.add(faListAlt, faHeading, faMoneyBillAlt, faBuilding);
 
-export default class VideoCard extends PureComponent {
+const VideoCard = ({
+  title,
+  embedUrl,
+  description,
+  sharedByEmail,
+  movieID,
+}) => {
+  const [liked, setLiked] = useState(false);
 
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  handleLikeMovie = (movieID) => {
+  const handleLikeMovie = (movieID) => {
     const params = {
       movie_id: movieID,
       reaction_type: 0
+    };
+    MovieAPI.likeMovie(params, UserUtils.getAccessToken())
+      .then(res => {
+        if (res.status === 201) {
+          setLiked(true);
+          alert("Liked");
+        }
+      })
+      .catch(e => console.log("error", e));
+  };
+
+  const checkUserLikedMovie = (movieID) => {
+    // Your logic to check if the user liked the movie
+    return false;
+  };
+
+  const renderButtonLike = (movieID) => {
+    if (!checkUserLikedMovie(movieID)) {
+      return (
+        <Button variant="contained" color="secondary" onClick={() => handleLikeMovie(movieID)}>
+          <ThumbUpIcon>
+            Like
+          </ThumbUpIcon>
+        </Button>
+      );
+    } else {
+      return (
+        <Button variant="contained" color="secondary" onClick={() => handleLikeMovie(movieID)}>
+          <ThumbUpIcon>
+            Liked
+          </ThumbUpIcon>
+        </Button>
+      );
     }
-    MovieAPI.likeMovie(params, UserUtils.getAccessToken()).then(res => {
-      if (res.status === 201) {
-        alert("Liked")
-      };
-    }).catch(e => console.log("error"));
-  }
+  };
 
-  checkUserLikedMovie = (movieID) => {
-    return false
-  }
-
-  renderButtonLike = () => {
-    if (!this.checkUserLikedMovie()) {
-      return(
-        <button onClick={
-          () => this.handleLikeMovie(movieID)
-        }>
-          Like
-        </button>
-      )
-    }
-    return(
-      <button onClick={
-        () => this.handleLikeMovie(movieID)
-      }>
-        Liked
-      </button>
-    )
-  }
-
-  render() {
-    const {
-      title,
-      embedUrl,
-      description,
-      sharedByEmail,
-      movieID
-    } = this.props;
-    console.log("$$$$3#####", this.props)
-
-    return (
-      <div className="VideoCard">
-        <Card className="VideoCard-container">
-          <div className="col-md-6">
-            <iframe className="VideoCard-cardImg" width="420" height="300" src={embedUrl}></iframe>
-          </div>
-          <CardBody className="col-md-6">
-            <CardTitle>
-              <span className="VideoCard-title">{title}</span>
-            </CardTitle>
-            <CardTitle><span>{`Shared by: ${sharedByEmail}`}</span></CardTitle>
-            {
-              <button onClick={
-                () => this.handleLikeMovie(movieID)
-              }>
-                Like
-              </button>
-            }
-            <CardTitle>Description: <br /><span>{description}</span></CardTitle>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="VideoCard">
+      <Card className="VideoCard-container">
+        <div className="col-md-6">
+          <iframe className="VideoCard-cardImg" width="420" height="300" src={embedUrl}></iframe>
+        </div>
+        <CardBody className="col-md-6">
+          <CardTitle>
+            <span className="VideoCard-title">{title}</span>
+          </CardTitle>
+          <CardTitle><span>{`Shared by: ${sharedByEmail}`}</span></CardTitle>
+          {renderButtonLike(movieID)}
+          <CardTitle>Description: <br /><span>{description}</span></CardTitle>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
 
 VideoCard.propTypes = {
   title: PropTypes.string,
@@ -91,5 +84,6 @@ VideoCard.propTypes = {
   sharedByEmail: PropTypes.string,
   embedUrl: PropTypes.string,
   movieID: PropTypes.any,
-  currentUser: PropTypes.any
 };
+
+export default VideoCard;
